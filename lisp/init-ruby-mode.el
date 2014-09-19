5a1465931c2077ecd96c37ffae1804d9d1e082bc
Sergio Garcia <sergio@demarque.com>
2014-09-19 09:37:38 -0400

    removing ac for ruby.

--- lisp/init-ruby-mode.el
+++ lisp/init-ruby-mode.el
@@ -28,12 +28,14 @@
 
 ;;; Inferior ruby
 (require-package 'inf-ruby)
-(require-package 'ac-inf-ruby)
-(after-load 'auto-complete
-  (add-to-list 'ac-modes 'inf-ruby-mode))
-(add-hook 'inf-ruby-mode-hook 'ac-inf-ruby-enable)
-(after-load 'inf-ruby
-  (define-key inf-ruby-mode-map (kbd "TAB") 'auto-complete))
+
+;;; dont load autocomplete
+;;(require-package 'ac-inf-ruby)
+;;(after-load 'auto-complete
+;;  (add-to-list 'ac-modes 'inf-ruby-mode))
+;;(add-hook 'inf-ruby-mode-hook 'ac-inf-ruby-enable)
+;;(after-load 'inf-ruby
+;;  (define-key inf-ruby-mode-map (kbd "TAB") 'auto-complete))
 
 
 
