519db6b390f8a95507b821037ba375824464ca69
sergio garcia <sergio.garcia@gmail.com>
2018-04-28 15:32:29 -0400

    emmet

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -9,36 +9,13 @@
 (add-hook 'text-mode-hook 'turn-on-auto-fill) ;; Set auto-fill-mode
 (add-hook 'html-erb-mode-hook 'turn-off-auto-fill)
 
-;; some crap to fix a pasteboard problem in mac:
+;; fix a pasteboard problem in mac:
 (when (eq window-system 'ns)
   (defadvice ns-get-pasteboard (around hack-empty-pasteboard compile activate)
     (condition-case err
         ad-do-it
       (quit (message "%s" (cadr err))
             nil))))
-
-
-(setq jabber-account-list
-      '(("sergio@demarque.com"
-         (:network-server . "talk.google.com")
-         (:connection-type . ssl)
-         (:port . 443)
-         (:password)
-         )
-        ("sagc@gozque.com"
-         (:network-server . "talk.google.com")
-         (:connection-type . ssl)
-         (:port . 443))
-        ))
-
-(setq jabber-auto-reconnect t)
-(setq jabber-roster-line-format "  %c %-25n %u %-8s ")
-(setq jabber-roster-show-bindings nil)
-(setq jabber-roster-show-title nil)
-(setq jabber-show-resources t)
-(setq jabber-chat-buffer-show-avatar nil)
-(add-hook 'jabber-post-connect-hooks 'jabber-autoaway-start)
-(set-face-attribute 'default nil :height 140)
 
 (defun setup-tide-mode ()
   (interactive)
@@ -60,5 +37,8 @@
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 
 (provide 'init-local)
